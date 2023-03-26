1dacd36ba689cc7913373b992e98fc9230f584f4
Spence Wetjen <swetjen@gmail.com>
2023-03-25 19:02:01 -0500

    Svelte + Tailwind working

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Counter from './Counter.svelte';
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import Button from '../components/Button.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1 class="text-5xl text-center">Hello world</h1>
+	<Button />
+	<Counter />
+</section>
