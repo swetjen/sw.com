b3ce7cf51ce0e2400a083065b47958be56a0a606
Spence Wetjen <swetjen@gmail.com>
2023-03-30 11:26:13 -0500

    svelte static

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,10 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true
+		})
 	},
 	preprocess: vitePreprocess()
 };
