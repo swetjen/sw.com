eaa4da7f441cf07abd591b60d9ccb476d7d94ee2
Spence Wetjen <swetjen@gmail.com>
2023-03-31 17:06:10 -0500

    go embed build artifacts

--- api/main.go
+++ api/main.go
@@ -2,61 +2,56 @@ package main
 
 import (
 	"embed"
+	"io/fs"
 	"log"
 	"net/http"
-	"text/template"
+	"net/url"
+	"strings"
 )
 
 // https://blog.jetbrains.com/go/2021/06/09/how-to-use-go-embed-in-go-1-16/
 
-var (
-	//go:embed build
-	res   embed.FS
-	pages = map[string]string{
-		"/": "build/index.html",
-	}
-)
+//go:embed all:build/*
+var build embed.FS
 
 func main() {
-	err := run()
+	contentRoot, _ := fs.Sub(build, "build")
+	fs := http.FileServer(http.FS(contentRoot))
+	//fs := http.FileServer(http.Dir("./build"))
+	//fs := http.FileServer(http.FS(build))
+	http.Handle("/", Adapter("/", fs))
+
+	log.Print("Listening on :3000...")
+	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func run() error {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		page, ok := pages[r.URL.Path]
-		if !ok {
-			w.WriteHeader(http.StatusNotFound)
-			return
-		}
-
-		tpl, err := template.ParseFS(res, page)
-		if err != nil {
-			log.Printf("page %s not found in pages cache...", r.RequestURI)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		w.Header().Set("Content-Type", "text/html")
-		w.WriteHeader(http.StatusOK)
-		data := map[string]interface{}{
-			"userAgent": r.UserAgent(),
-		}
-
-		if err := tpl.Execute(w, data); err != nil {
-			return
-		}
-
-	})
-	http.FileServer(http.FS(res))
-
-	log.Println("Server started...")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		return err
+// Adapter adds html suffix is the request lacks an extension
+func Adapter(prefix string, h http.Handler) http.Handler {
+	if prefix == "" {
+		return h
 	}
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		p := strings.TrimPrefix(r.URL.Path, prefix)
+		rp := strings.TrimPrefix(r.URL.RawPath, prefix)
+		if len(p) < len(r.URL.Path) && (r.URL.RawPath == "" || len(rp) < len(r.URL.RawPath)) {
 
-	return nil
+			// append html
+			if len(p) > 0 && !strings.Contains(p, ".") {
+				p = p + ".html"
+			}
+
+			r2 := new(http.Request)
+			*r2 = *r
+			r2.URL = new(url.URL)
+			*r2.URL = *r.URL
+			r2.URL.Path = p
+			r2.URL.RawPath = rp
+			h.ServeHTTP(w, r2)
+		} else {
+			http.NotFound(w, r)
+		}
+	})
 }
