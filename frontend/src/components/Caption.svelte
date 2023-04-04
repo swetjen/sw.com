b9a82acf12a958edc260037264aff92243aac44d
Spence Wetjen <swetjen@gmail.com>
2023-04-03 21:04:40 -0500

    Initial commit

---
+++ frontend/src/components/Caption.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+
+    export let props: string
+
+</script>
+
+<div class="max-w-2xl my-2 mx-auto">
+    <p class="font-medium text-sm text-gray-600 leading-5 tracking-tighter">
+        {props}
+    </p>
+</div>
