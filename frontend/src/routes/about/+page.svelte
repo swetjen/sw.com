dc4d8e20dba8a3e08d5d347c770acecb43e091d3
Spence Wetjen <swetjen@gmail.com>
2023-04-25 22:01:18 -0500

    About page; self portrait

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -1,6 +1,45 @@
+<script>
+  import Image from '../../components/Image.svelte';
+</script>
+
 <svelte:head>
-	<title>About</title>
-	<meta name="description" content="About this app" />
+    <title>About</title>
+    <meta name="description" content="About this app"/>
 </svelte:head>
 
-<p class="">todo</p>
+
+<section>
+    <div class="my-10 max-w-2xl mx-auto">
+        <div class="mx-5 space-y-8">
+            <h2>About me</h2>
+            <p>
+                Hi, I'm Spence Wetjen - a full-stack developer and UX designer based in Austin, Texas, USA. With 15
+                years of experience in the e-commerce, software-as-a-service, and web industries, I have had the
+                privilege of working with some of the world's leading organizations such as Microsoft, Google, Apple,
+                Best Buy, and Cisco.
+            </p>
+            <p>
+                Over the years, I have worked on both large and small projects, gaining extensive experience across a
+                variety of project types and working with teams of various sizes, each with their own constraints and
+                goals. Most of my projects involve new product development, technology updates, user experience
+                refreshes, and web service integrations.
+            </p>
+            <p>
+                Overall, I apply my experience to help teams launch their products quickly. My background frequently
+                helps to guide teams towards success while avoiding unexpected issues. Throughout the design and
+                development process, I prioritize clear goals, communication, user-centered-design, rapid prototyping,
+                and collaboration.
+            </p>
+            <h2>Profile Picture</h2>
+            <p>
+                Me and my good friend Talulah. :)
+
+            </p>
+            <div class="max-w-xs mx-auto">
+                <Image class="max-w-sm mx-auto" src="/SpenceWetjen.JPG" alt="Spence Wetjen and best-dog-friend Talulah."/>
+            </div>
+        </div>
+    </div>
+
+
+</section>
