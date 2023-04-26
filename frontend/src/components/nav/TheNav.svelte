12af17c1941100e80a039fe98c6e912d95c4a557
Spence Wetjen <swetjen@gmail.com>
2023-04-25 20:54:57 -0500

    Link

--- frontend/src/components/nav/TheNav.svelte
+++ frontend/src/components/nav/TheNav.svelte
@@ -112,7 +112,7 @@
         <div class="fixed inset-0 z-10"></div>
         <div class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div class="flex items-center justify-between">
-                <a href="#" class="-m-1.5 p-1.5">
+                <a href="/" class="-m-1.5 p-1.5">
                     <span class="sr-only">Spence Wetjen</span>
                     <h1 aria-description="Spence Wetjen">Spence Wetjen</h1>
                 </a>
