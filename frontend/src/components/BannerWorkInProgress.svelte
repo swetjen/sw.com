45775ca04109998ba785eeed5c99de4cf04c0c80
Spence Wetjen <swetjen@gmail.com>
2023-04-25 20:56:28 -0500

    copy

--- frontend/src/components/BannerWorkInProgress.svelte
+++ frontend/src/components/BannerWorkInProgress.svelte
@@ -7,7 +7,7 @@
         <div class="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30" style="clip-path: polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)"></div>
     </div>
     <p class="text-sm leading-6 text-gray-900">
-        👋<svg viewBox="0 0 2 2" class="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true"><circle cx="1" cy="1" r="1" /></svg>This site is a work in progress. Missing content coming soon!
+        👋<svg viewBox="0 0 2 2" class="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true"><circle cx="1" cy="1" r="1" /></svg>This site is a work in progress. Missing content is coming soon.
     </p>
     <div class="flex flex-1 justify-end">
 <!--        <button type="button" class="-m-3 p-3 focus-visible:outline-offset-[-4px]">-->
