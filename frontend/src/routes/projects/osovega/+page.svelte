61c150b995430ae99c6e47c9becd8c8073176ff2
Spence Wetjen <swetjen@gmail.com>
2023-04-26 21:23:01 -0500

    Shim osovega

---
+++ frontend/src/routes/projects/osovega/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import type {IProjectItem} from '../../../interfaces/all';
+  import ProjectHero from '../../../components/projects/ProjectHero.svelte';
+  import TheBread from '../../../components/TheBread.svelte';
+
+  import {Projects} from '../../../stores/projects';
+  import {page} from '$app/stores';
+  const tokens = $page.url.pathname.split('/');
+  const thisProject: IProjectItem = $Projects.find((p) => p.slug.includes(tokens[tokens.length -1]))
+
+
+</script>
+
+<svelte:head>
+    <title>Spence Wetjen</title>
+    <meta name="description" content="Spence Wetjen | Full Stack Developer"/>
+</svelte:head>
+
+<section class="my-10">
+    <ProjectHero project={thisProject}/>
+</section>
+
+<section class="my-10 space-y-8">
+    <p>todo</p>
+</section>
