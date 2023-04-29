8b4fe023ed30a5070054542080d84d98b6e841b0
Spence Wetjen <swetjen@gmail.com>
2023-04-29 17:19:39 -0500

    Formatting; add Postgres; safe lookup

--- frontend/src/components/TechLogo.svelte
+++ frontend/src/components/TechLogo.svelte
@@ -144,11 +144,20 @@
 </g>
 `
         },
+        {
+            viewBox: "261.76 226.69",
+            name: "Vue",
+            svg: `
+<g transform="matrix(1.3333 0 0 -1.3333 -76.311 313.34)"><g transform="translate(178.06 235.01)"><path d="m0 0-22.669-39.264-22.669 39.264h-75.491l98.16-170.02 98.16 170.02z" fill="#41b883"/></g><g transform="translate(178.06 235.01)"><path d="m0 0-22.669-39.264-22.669 39.264h-36.227l58.896-102.01 58.896 102.01z" fill="#34495e"/></g></g>
+`
+        },
     ];
     let displayIcon = logos.find((e) => e.name === name);
 </script>
+{#if displayIcon}
 <svg
         class={$$props.class}
         {focusable}
         viewBox="0 0 {displayIcon.viewBox}">{@html displayIcon.svg}
 </svg>
+{/if}
