b9397ebb1f6e641702b5097de9e03a0fe4817955
Spence Wetjen <swetjen@gmail.com>
2023-05-02 08:48:35 -0500

    Add plausible

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -6,6 +6,11 @@
     import TechLogo from "../components/TechLogo.svelte";
 </script>
 
+<svelte:head>
+    <script defer data-domain="spencewetjen.com" src="https://plausible.io/js/script.js"></script>
+</svelte:head>
+
+
 <div class="">
     <TheNav/>
     <ContactModal/>
